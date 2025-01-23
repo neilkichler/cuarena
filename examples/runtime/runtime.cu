633bcd8512b9469f93b381a5735172819485ad45
Neil Kichler <neil.kichler@gmail.com>
2025-01-23 14:51:18 +0100

    add runtime example

---
+++ examples/runtime/runtime.cu
@@ -0,0 +1,51 @@
+#include <cuarena/cuarena.hpp>
+
+#include <cstdio>
+
+#define CU_CHECK(x)                                                                               \
+    do {                                                                                          \
+        CUresult err = x;                                                                         \
+        if (err != CUDA_SUCCESS) {                                                                \
+            const char *error_string;                                                             \
+            if (cuGetErrorString(err, &error_string) == CUDA_ERROR_INVALID_VALUE) {               \
+                error_string = "Unknown error";                                                   \
+            }                                                                                     \
+            const char *error_name;                                                               \
+            if (cuGetErrorName(err, &error_name) == CUDA_ERROR_INVALID_VALUE) {                   \
+                error_name = "CUDA_ERROR_UNKNOWN";                                                \
+            }                                                                                     \
+            fprintf(stderr, "CUDA Driver API error in %s at %s:%d: %s (%s = %d)\n", __FUNCTION__, \
+                    __FILE__, __LINE__, error_string, error_name, err);                           \
+            abort();                                                                              \
+        }                                                                                         \
+    } while (0)
+
+__global__ void kernel(int *xs) { xs[0] = 42; }
+
+int main()
+{
+
+    cudaSetDevice(0);
+    CUcontext ctx;
+    CU_CHECK(cuCtxGetCurrent(&ctx));
+    // printf("device is : %d\n", dev);
+
+    constexpr int n = 1024;
+    int h_xs[n];
+
+    auto n_bytes = sizeof(int) * n;
+    // arena a(dev, 8_GB);
+    arena a(ctx, 8_GB);
+    // arena a;
+
+    memblk buffer = a.allocate(n_bytes);
+    int *d_xs     = new (buffer.data()) int[n];
+
+    kernel<<<1, 1>>>(d_xs);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_xs, d_xs, n_bytes, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    printf("h_xs[0] = %d\n", h_xs[0]);
+    return 0;
+}
